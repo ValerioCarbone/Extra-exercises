2ca24896e86d9ce92cbe6ad29d9ae0baa2afbf28
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-28 15:47:41 +0100

    first commit

---
+++ Ex-01/Ex-01.js
@@ -0,0 +1,30 @@
+
+const word = "Marco";
+
+const lowerWord = word.toLowerCase();
+
+let Arr = lowerWord.split("");
+
+let new_word;
+
+for (let i = 0; i < lowerWord.length; i++) {
+
+    if (Arr[i] == 'a') {
+        Arr.splice(i, 1, 'afa')
+    } else if (Arr[i] == 'e') {
+        Arr.splice(i, 1, 'efe')
+    } else if (Arr[i] == 'i') {
+        Arr.splice(i, 1, 'ifi')
+    } else if (Arr[i] == 'o') {
+        Arr.splice(i, 1, 'ofo')
+    } else if (Arr[i] == 'u') {
+        Arr.splice(i, 1, 'ufu')
+    }
+}
+
+new_word = Arr.join('')
+
+console.log(new_word)
+
+
+ABCDEABCDEABCDEABCDE
